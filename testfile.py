e0c595a7406dc4748593b779fee87a1110b6dbb3
Maya Bhat <mayabhat@andrew.cmu.edu>
2020-04-06 21:05:40 -0400

    commit practice

--- testfile.py
+++ testfile.py
@@ -3,5 +3,3 @@ print('This is the second line of the test file document')
 print('This is the second line of the test file document')
 print('poop')
 print('No poopoo')
- e84eb0e45d3229f3935d6df78c609633ab935c77
-
